b1be0116e113695cb2116e05366f3578e0d4f91b
ehernandez <ehernandez@xumak.com>
2017-01-31 08:22:33 -0600

    Now send/receive JSON instead of []bytes, this allows us to send/receive more info about the client

--- chat/room.go
+++ chat/room.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/ehernandez-xk/bp_app/trace"
 	"github.com/gorilla/websocket"
+	"github.com/stretchr/objx"
 )
 
 const (
@@ -18,7 +19,7 @@ var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBuffer
 type room struct {
 	// forward is a channel that holds incomming messages
 	// that should be forwarded to the other clients.
-	forward chan []byte
+	forward chan *message
 	// join is a channel for clients wishing to join the room
 	join chan *client
 	// leave is a channel for clients wishing to leave the room
@@ -33,7 +34,7 @@ type room struct {
 // newRoom makes a new room
 func newRoom() *room {
 	return &room{
-		forward: make(chan []byte),
+		forward: make(chan *message),
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
@@ -46,10 +47,16 @@ func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		log.Fatal("ServeHTTP", err)
 		return
 	}
+	authCookie, err := req.Cookie("auth")
+	if err != nil {
+		log.Fatal("Failed to get auth cookie:", err)
+		return
+	}
 	client := &client{
-		socket: socket,
-		send:   make(chan []byte, messageBufferSize),
-		room:   r,
+		socket:   socket,
+		send:     make(chan *message, messageBufferSize),
+		room:     r,
+		userData: objx.MustFromBase64(authCookie.Value),
 	}
 	r.join <- client
 	defer func() { r.leave <- client }()
@@ -74,7 +81,7 @@ func (r *room) run() {
 			// forward message to all clients
 			for client := range r.clients {
 				client.send <- msg
-				r.tracer.Trace(" -- sent to client")
+				r.tracer.Trace("Message received:", msg.Message)
 			}
 		}
 	}
