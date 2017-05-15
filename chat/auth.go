7fb327ce72063626d353d9ac49645db039a517d9
ehernandez <ehernandez@xumak.com>
2017-05-15 15:24:07 -0600

    Added image avatar from github

--- chat/auth.go
+++ chat/auth.go
@@ -84,7 +84,8 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 
 		// setup the Cookie
 		authCookieValue := objx.New(map[string]interface{}{
-			"name": user.Name(),
+			"name":       user.Name(),
+			"avatar_url": user.AvatarURL(),
 		}).MustBase64()
 		http.SetCookie(w, &http.Cookie{
 			Name:  "auth",
